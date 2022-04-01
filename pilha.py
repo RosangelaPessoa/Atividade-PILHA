c0131e88f40e04c475abe3d27146275c51546601
Rosangela Pessoa <89878556+RosangelaPessoa@users.noreply.github.com>
2022-04-01 17:28:56 -0300

    Create pilha.py

---
+++ pilha.py
@@ -0,0 +1,39 @@
+       expressão = input("Agora digite a sequência de caracteres, para validar sua expressão: ")
+x = 0
+pilha = []
+while x < len(expressão):
+    if expressão[x] == "(":
+        pilha.append("(")
+
+    if expressão[x] == "[":
+        pilha.append("[")
+
+    if expressão[x] == "{":
+        pilha.append("{")
+
+    if expressão[x] == ")":
+        if len(pilha) > 0:
+            topo = pilha.pop(-1)
+        else:
+            pilha.append(")")  # Força a mensagem de erro
+            break
+
+    if expressão[x] == "]":
+        if len(pilha) > 0:
+            topo = pilha.pop(-1)
+        else:
+            pilha.append("]")  # Força a mensagem de erro
+            break
+
+    if expressão[x] == "}":
+        if len(pilha) > 0:
+            topo = pilha.pop(-1)
+        else:
+            pilha.append("}")  # Força a mensagem de erro
+            break
+
+    x = x + 1
+if len(pilha) == 0:
+    print("Parabéns", nome,"! Sua Expressão está correta!")
+else:
+    print("Poxa", nome,"! Sua Expressão NÃO está correta!")
